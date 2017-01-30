c5689975247d65ef099d5b3df93403beab6f27d3
Richard Dotchin <richard.dotchin@gmail.com>
2017-01-29 21:52:10 -0500

    changed connection.js for jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
 /*=======================Connect Node to MySQL==============================*/
 //require mysql npm package
-const mysql = require('mysql');
-
+var mysql = require('mysql');
+var connection;
 //create connection to burgers_db set to variable named connection
-const connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) { 
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+	var connection = mysql.createConnection({
   			host     : 'localhost',
   			user     : 'root',
   			password : '',
   			database : 'burgers_db'
 	});
+}
+
 
 //establish a connection
 connection.connect(function(err){
 	if(err){
-		console.log('error connection: ' + err.stack)
+		console.log('error connection: ' + err.stack);
 	}
 	console.log('connected to mysql as id ' + connection.threadId);
 });
